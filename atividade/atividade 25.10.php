bf7c364c582eaa08a7d44d63f735b140bec035a4
zDESTROYERz0 <146986547+zDESTROYERz0@users.noreply.github.com>
2023-10-25 14:59:51 -0300

    Create atividade 25.10.php

---
+++ atividade/atividade 25.10.php
@@ -0,0 +1,12 @@
+<?php
+//NÚMEROS PARES 0-500// 
+for ($i = 0; $i <= 500; $i += 2) {
+    echo $i . " ";
+}
+
+//NÚMEROS ÍMAPRES 500-0//
+for ($i = 499; $i >= 1; $i -= 2) {
+    echo $i . " ";
+}
+?>
+
